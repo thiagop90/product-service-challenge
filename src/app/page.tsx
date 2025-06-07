27ce6d50e7d3529ee92d200bdce15b37e9ce4525
Thiago <thgps01@outlook.com>
2025-06-07 12:37:24 -0300

    feat: add button on home page to create a product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { ProductCreateForm } from '@/components/form/product-create-form'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <main className="max-w-3xl mx-auto px-6 py-10 sm:px-10 min-h-dvh border-x">
+      <div className="pb-2 border-b flex justify-between">
+        <h2 className="text-2xl sm:text-3xl font-semibold tracking-tight">
+          Product Service
+        </h2>
+
+        <ProductCreateForm />
+      </div>
+    </main>
+  )
 }
