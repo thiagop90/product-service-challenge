06ff20f477bdce1df2325862e8b8c771c136c65f
Thiago <thgps01@outlook.com>
2025-06-07 21:17:10 -0300

    feat: add data fetching with React Query to products display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+'use client'
+
 import { ProductCreateForm } from '@/components/form/product-create-form'
+import { getProducts } from '@/services/product-service'
+import { useQuery } from '@tanstack/react-query'
 
 export default function Home() {
+  const { data } = useQuery({
+    queryKey: ['products'],
+    queryFn: getProducts,
+  })
+
   return (
     <main className="max-w-3xl mx-auto px-6 py-10 sm:px-10 min-h-dvh border-x">
       <div className="pb-2 border-b flex justify-between">
@@ -9,6 +18,14 @@ export default function Home() {
         </h2>
 
         <ProductCreateForm />
+      </div>
+
+      <div className="py-6">
+        <pre className="w-full rounded-md bg-muted p-4 overflow-x-auto">
+          <code className="text-sm sm:text-base">
+            {JSON.stringify(data, null, 2)}
+          </code>
+        </pre>
       </div>
     </main>
   )
